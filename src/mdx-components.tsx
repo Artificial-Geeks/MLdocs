4978c380b385deeef9fbe008ff38e671fffdda20
MasterBhuvnesh <bhuvneshverma2904@gmail.com>
2025-09-18 22:59:13 +0530

    docs:updated content of labs

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,3 +1,4 @@
+import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import * as icons from 'lucide-react';
@@ -7,6 +8,8 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...(icons as unknown as MDXComponents),
     ...defaultMdxComponents,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    img: (props) => <ImageZoom {...(props as any)} />,
     ...components,
   };
 }
